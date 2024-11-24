e79542013cae2aa01a1cbe050241c1ccca9713a2
andy0427s <andy0427s@gmail.com>
2024-11-24 19:30:31 +0800

    build frontend to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
+const fs = require('fs');
+const path = require('path');
 
 
 const app = express();
@@ -9,6 +11,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 // Routes
 app.use('/api/users', require('./routes/users'));
@@ -19,7 +22,12 @@ app.use('/api/rooms', require('./routes/rooms'));
 app.use('/api/leaderboards', require('./routes/leaderboards'));
 app.use('/api/chats', require('./routes/chats'));
 
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 // Error handling middleware
 app.use(errorHandler);
+
 
 module.exports = app;
